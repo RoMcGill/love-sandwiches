4c4cf0d1af66f9e635acf0bb628a9755cceb5219
RoMcGill <ro.mcgill12@gmail.com>
2022-03-30 16:26:52 +0000

    retrieve latest stock numbers

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -63,6 +65,27 @@ def update_sales_worksheet(data):
     print("sales worksheet updated successfully.\n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    compare sales and stock and calculate the surplus of each item
+    surplus = sales - stock
+    positive surplus indicates waste 
+    negative surplus = extras made when stock sold out 
+    """
+    print("calculating surplus data .... \n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    """
+    run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("welcome to ls data automation")
+main()
