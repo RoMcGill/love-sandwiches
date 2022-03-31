f193910fcf0cdd4d3f10e92ada11eb6325200547
RoMcGill <ro.mcgill12@gmail.com>
2022-03-31 13:29:30 +0000

    update all sheets

--- run.py
+++ run.py
@@ -122,6 +122,20 @@ def get_last_5_entries_sales():
     return columns
     
 
+def calculate_stock_data(data):
+    """
+    calculate avg stoock for each item
+    """
+    print ("calculating stock data")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int (num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average *1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 def main():
     """
@@ -132,9 +146,13 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("welcome to ls data automation")
-# main()
+main()
 
-slaes_columns = get_last_5_entries_sales()
+sales_columns = get_last_5_entries_sales()
+stock_data = calculate_stock_data(sales_columns)
