33c693c9ef21e888248378bb382ff96456234f3d
RoMcGill <ro.mcgill12@gmail.com>
2022-03-31 13:08:36 +0000

    collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -53,11 +53,11 @@ def validate_data(values):
         return False
 
     return True
-
+"""
 def update_sales_worksheet(data):
-    """
+    
     update sales worksheet add new row with list of data provided 
-    """
+    
 
     print ("updating worksheet  .... \n")
     sales_worksheet = SHEET.worksheet("sales")
@@ -65,14 +65,25 @@ def update_sales_worksheet(data):
     print("sales worksheet updated successfully.\n")
 
 def update_surplus_worksheet(data):
-    """
+    
     update surplus worksheet add new row with list of data provided 
-    """
+    
 
-    print ("updating surplus worksheet  .... \n")
+    print (f"updating surplus worksheet  .... \n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("surplus worksheet updated successfully.\n")
+"""
+
+def update_worksheet(data, worksheet):
+    """
+    update relevant worksheet 
+    """
+    print (f"updating {worksheet} worksheet  .... \n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 
 
 
@@ -95,6 +106,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    coleects collums of data from sales worksheet,
+    collecting the last 5 entries for each sandwich 
+    and returns the data as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+   
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+    
+
 
 def main():
     """
@@ -102,10 +129,12 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("welcome to ls data automation")
-main()
+# main()
+
+slaes_columns = get_last_5_entries_sales()
